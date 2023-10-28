af9c8385e666733a3a75774bd70eb66fafcc7989
dmvoytyuk <dmvoytyuk@gmail.com>
2023-10-28 23:52:48 +0200

    header with webp background + menu animation

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,14 @@
+function openMenu() {
+  console.log("clicked");
+  var menu = document.getElementById("mobile-menu-id");
+  menu.classList.remove("is-closed");
+  menu.classList.add("is-open");
+  
+}
+
+function closeMenu() {
+  console.log("clicked close button");
+  var menu = document.getElementById("mobile-menu-id");
+  menu.classList.remove("is-open");
+  menu.classList.add("is-closed");
+}
