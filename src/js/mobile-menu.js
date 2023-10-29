380c447de450d7621ff200583eb7001953ffd258
dmvoytyuk <dmvoytyuk@gmail.com>
2023-10-29 09:52:02 +0100

    mobile menu JS

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,34 @@
-function openMenu() {
-  console.log("clicked");
-  var menu = document.getElementById("mobile-menu-id");
-  menu.classList.add("is-open");  
+const menuCloseButton = document.getElementById('menu-close-button');
+const menuOpenButton = document.getElementById('menu-open-button');
+const menu = document.getElementById('mobile-menu-id');
+const menuLink = document.getElementsByClassName('mobile-nav-links-item');
+
+// show/hide the menu when the button is clicked
+for (let link of menuLink) {
+  // console.log(link);
+  link.addEventListener('click', () => {
+    console.log("clicked close button");
+    menu.classList.remove('is-open');
+  });
 }
 
-function closeMenu() {
+menuOpenButton.addEventListener('click', () => {
+  console.log(menu);
+  console.log('click open');
+  menu.classList.add('is-open');
+  console.log(menu);
+});
+
+menuCloseButton.addEventListener('click', () => {
+  console.log(menu);
   console.log("clicked close button");
-  var menu = document.getElementById("mobile-menu-id");
-  menu.classList.remove("is-open");
-}
+  menu.classList.remove('is-open');
+  console.log(menu);
+});
+
+// hide the menu when a click event occurs outside the menu
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target) && !menuOpenButton.contains(event.target)) {
+    menu.classList.remove('is-open');
+  }
+});
