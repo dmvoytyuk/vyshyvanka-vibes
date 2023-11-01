f9512677a7e0aa3988a0f0fac9a9dd860ffb97e3
dmvoytyuk <dmvoytyuk@gmail.com>
2023-11-01 01:08:00 +0100

    update js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,16 +1,16 @@
-const slidesContainer = document.getElementById("testimonials-list-id");
-const slide = document.querySelector(".testimonials-item");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+// const slidesContainer = document.getElementById("testimonials-list-id");
+// const slide = document.querySelector(".testimonials-item");
+// const prevButton = document.getElementById("slide-arrow-prev");
+// const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-  console.log("HELLO");
-  const slideWidth = slide.clientWidth + "40px";
-  slidesContainer.scrollLeft += slideWidth;
-});
+// nextButton.addEventListener("click", () => {
+//   console.log("HELLO");
+//   const slideWidth = slide.clientWidth + "40px";
+//   slidesContainer.scrollLeft += slideWidth;
+// });
 
-prevButton.addEventListener("click", () => {
-  console.log("HELLO");
-  const slideWidth = slide.clientWidth + "40px";
-  slidesContainer.scrollLeft -= slideWidth;
-});
+// prevButton.addEventListener("click", () => {
+//   console.log("HELLO");
+//   const slideWidth = slide.clientWidth + "40px";
+//   slidesContainer.scrollLeft -= slideWidth;
+// });
